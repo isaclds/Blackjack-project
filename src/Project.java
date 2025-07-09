dd73b5be453196e36a2301dc4c75f4525a1da6ff
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2025-07-09 15:16:38 -0300

    Adição de algumas variaveis para utilização, alteração da forma de gerar o baralho e quando ele é embaralhado, mudança na forma de distribuir cartas para melhor reutilização e todos switch cases trocados para rule switch.

--- src/Project.java
+++ src/Project.java
@@ -1,21 +1,23 @@
 import java.util.*;
 
 public class Project {
-    //Declaração das principais variaveis do projeto
+    //Declaração das principais variaveis do projeto e que seram acessadas em diversas das funções
     private static Baralho baralho;
     private static Scanner scan;
     private static double banca;
+    private static double bancaComp;
+    private static double aposta;
     private static boolean continuarJogando = true;
 
-    //Tudo feito em funções para melhor manutenção do jogo e diminuir possiveis confunções
+    //Tudo feito em funções para melhor manutenção do jogo e diminuir possiveis confusões
     public static void main(String[] args) {
         inicializarJogo();
         loopPrincipal();
+        fimDeJogo();
     }
 
     //Função de iniciar o jogo, declarando o baralho como um novo objeto da classe baralho e o mesmo do scanner, enquanto já embaralha e uma mensagem de boas vindas ao jogador
     private static void inicializarJogo(){
-        baralho = new Baralho();
         scan = new Scanner(System.in);
         System.out.println("Bem vindo ao Blackjack!");
     }
@@ -28,51 +30,70 @@ public class Project {
 
             //Switch case para iniciar o loop principal
             switch (escolha) {
-            case 1: 
+            case 1 ->{ 
               System.out.println("Insira o valor da sua banca!");
               banca = scan.nextDouble();
+              bancaComp = banca;
+              //Embaralhar o baralho
                 while(continuarJogando){
                     rodada();
                     }
-                break; 
-            case 2:
+                 }
+            case 2 -> {
                 System.out.println("Volte sempre!");
-                break;
-                
-            default:
-                throw new Error("Não foi possível encontrar essa opção.");
+            }
+            default ->{
+                System.out.println("Não foi possível encontrar essa opção. Tente novamente!");
+                loopPrincipal();
+            }
         }
     }
 
+     private static void fimDeJogo() {
+
+         System.out.println("Volte sempre!");
+         if(banca>bancaComp){
+            System.out.printf("Você ganhou R$%.2f!\n", banca-bancaComp);
+         }
+    }
+
     private static void rodada(){
+        //Cria um baralho novo a cada rodada e o embaralha
+        baralho = new Baralho();
+        baralho.embaralhar();
         List<Carta> maoJogador = new ArrayList<>();
         List<Carta> maoDealer = new ArrayList<>();
 
+        //Valor da aposta sempre setado em 0 no inicio da função para evitar futuros bugs de apostas não zeradas
+        aposta = 0;
+        aposta = apostar();
         //Ao apostar o valor da aposta já é retirado da banca
-        double aposta = apostar();
         banca -= aposta;
 
-        distribuirCartas(maoJogador, maoDealer);
+        //Comprar carta de forma única para que na parte de decisão eu posso apenas reutilizar essa função
+        comprarCarta(maoDealer);
+        comprarCarta(maoJogador);
+        comprarCarta(maoDealer);
+        comprarCarta(maoJogador);
 
-        //Mostra as cartas do jogador
-        System.out.printf("Sua mão é %s e %s\n", maoJogador.get(0), maoJogador.get(1));
-        // Mostra as cartas do Dealer
-        System.out.printf("A mão do dealer é: %s e %s\n", maoDealer.get(0), maoDealer.get(1));
-
-        //Pega a quantidade de pontos do jogador e do Dealer e vê se alguém ganhou e informa ao jogador, facilitar a vida dele
+        //Pega a quantidade de pontos do jogador e do Dealer
         int pontosJogador = calculadoraPontos(maoJogador);
         int pontosDealer = calculadoraPontos(maoDealer);
-        System.out.printf("O dealer tem %d pontos e você tem %d pontos.\n", pontosDealer, pontosJogador);
-        banca = verificarVencedor(pontosJogador, pontosDealer, banca, aposta);
-        // aposta = 0;
 
-        //Perguntar se o jogador quer mais cartas, desistir, dobrar, parar
+        //Mostra as cartas do jogador e fala seus pontos
+        System.out.printf("Sua mão é %s e %s, você tem %d pontos.\n", maoJogador.get(0), maoJogador.get(1), pontosJogador);
+        // Mostra as cartas do Dealer
+        System.out.printf("A mão do dealer é: %s e %s, ele tem %d pontos.\n", maoDealer.get(0), maoDealer.get(1), pontosDealer);
+
+        //Verifica se alguém ganhou com um Blackjack
+        verificarVencedorPorVU(pontosJogador, pontosDealer);
+
+        // decisaoRodada(maoJogador, maoDealer);
 
     }
 
     private static double apostar(){
         boolean apostaValida = false;
-        double aposta = 0;
         
         //Lop para checagem se a aposta é válida ou não, feito separado porque será reutilizado durante o projeto
         while (!apostaValida) {
@@ -84,25 +105,29 @@ public class Project {
             } else if (aposta <= 0) {
                 System.out.println("Valor deve ser positivo!");
             } else {
+                System.out.println("Bom jogo!");
                 apostaValida = true;
             }
         }
         return aposta;
     }
 
-    private static void distribuirCartas(List<Carta> maoJogador, List<Carta> maoDealer){
-        baralho.embaralhar();
-        maoJogador.add(baralho.selecionarCarta());
-        maoDealer.add(baralho.selecionarCarta());
-        maoJogador.add(baralho.selecionarCarta());
-        maoDealer.add(baralho.selecionarCarta());
+    // private static void distribuirCartas(List<Carta> maoJogador, List<Carta> maoDealer){
+    //     maoJogador.add(baralho.selecionarCarta());
+    //     maoDealer.add(baralho.selecionarCarta());
+    //     maoJogador.add(baralho.selecionarCarta());
+    //     maoDealer.add(baralho.selecionarCarta());
+    // }
+
+     private static void comprarCarta(List<Carta> mao){
+        mao.add(baralho.selecionarCarta());
     }
 
     private static int calculadoraPontos(List<Carta> mao){
         int pontos = 0;
 
         //Classe map para atribuir valor as cartas que são letras
-        Map<String, Integer> valorCartaLetra = new HashMap<String, Integer>();
+        Map<String, Integer> valorCartaLetra = new HashMap<>();
         valorCartaLetra.put("J", 10);
         valorCartaLetra.put("Q", 10);
         valorCartaLetra.put("K", 10);
@@ -135,45 +160,80 @@ public class Project {
         return pontos;
     }
 
-    private static double verificarVencedor(int pontosJogador, int pontosDealer, double banca, double aposta){
+    private static void verificarVencedorPorVU(int pontosJogador, int pontosDealer){
         //Ver se empatou em 21, caso sim valor da aposta volta para a banca
         if((pontosJogador == 21) && (pontosDealer == 21)){
-
-            //Zera as apostas e retorna o valor para banca
-            banca += aposta;
-
-            System.out.println("Houve um empate, apostas foram zeradas.");
-            //Verifica se o jogador deseja continuar jogando
-            continuarJogo();
+            empate();
         } else if(pontosJogador == 21){
             //Jogador tirou um blackjack e verifica se ele deseja continuar jogando
             System.out.println("Parabéns! Você tem um Blackjack!");
-            banca = banca +(aposta * 3/2);
+            banca = banca +(aposta * 2) + ( aposta * 3/2);
             continuarJogo();
+
         } else if(pontosDealer == 21){
+            //Dealer tem um blackjack, jogador perdeu
             System.out.println("O Dealer tem um Blackjack! Que azar...");
             continuarJogo();
+
         }
-    return banca;
     }
 
     private static void continuarJogo(){
          System.out.println("Deseja continuar jogando? \n 1 - Sim\n 2 - Não");
             int escolha = scan.nextInt();
             switch(escolha){
-                case 1: 
-                break; 
-            case 2:
-                System.out.println("Volte sempre!");
+                case 1 -> {
+            }
+            case 2 -> {
                 continuarJogando = false;
-                break;
-            default:
+            }
+            default -> {
                 System.out.println("Opção inválida! Tente novamente.");
                 continuarJogo();
             }
+            }
     }
 
+    private static void decisaoRodada(List<Carta> maoJogador, List<Carta> maoDealer){
+        int pontosDealer = calculadoraPontos(maoDealer);
+        int pontosJogador = calculadoraPontos(maoJogador);
+          //Verificar se o dealer já tem 17 pontos, se ele não tiver ele é obrigado a chegar no minimo a 17
+        while(pontosDealer < 17){
+            comprarCarta(maoDealer);
+        }
+        //Mais cartas
+
+        //Desistir
+
+        //Dobrar
+
+        //Parar
+
+    }
+
+//Situações de pontos
+
+    private static void empate(){
+        //Zera as apostas e retorna o valor para banca
+        banca += aposta;
+
+        System.out.println("Houve um empate, as apostas foram zeradas.");
+        //Verifica se o jogador deseja continuar jogando
+         continuarJogo();
+    }
+
+    // if(pontosDealer>pontosJogador){
+            
+    //         System.out.println("O Dealer ganhou. Mais sorte da próxima vez!");
+    //         continuarJogo();
+
+    //     } else if(pontosDealer<pontosJogador){
+    //         System.out.println("Você ganhou! Parabéns!");
+    //         banca = banca + (aposta * 2);
+    //         continuarJogo();
+
+    //     }
+
     //Melhorar o verificar vencedor, para situações que ninguem tenha atingido 21, esse tipo de coisa
-    //Dar continuidade as funcionalidades para o jogador receber mais uma carta, poder dobrar a aposta desistir, ou esperar para ver o que o dealer tem
     //Lembrar dq o dealer sempre ira trazer mais cartas até que ele some 17 pontos. As coisas tem que acontecer ao mesmo tempo
 }
